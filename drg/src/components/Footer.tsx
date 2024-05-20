d61c2c39a28b81911add15a6f3d1ef8302c6adeb
Teishotoku <Dl1SqP1L4F0he@protonmail.com>
2024-05-20 14:02:11 +0200

    up

--- drg/src/components/Footer.tsx
+++ drg/src/components/Footer.tsx
@@ -2,6 +2,11 @@ const Footer = () => {
   
   return (
   <footer>
+			<div>
+				<p>jeast streat</p>
+				<p><b>GHOST SHIP GAMES</b></p>
+				<p>phone: 543-912</p>
+			</div>
       
     </footer> 
   );
--- drg/src/components/Shop.tsx
+++ drg/src/components/Shop.tsx
@@ -1,6 +1,31 @@
 const Shop = () => {
   return (
   <div className="Shop">
+			<div className="upping">Buy now!</div>
+			<div className="editions">
+
+				<div className="edit">
+					<div className="classic">
+						<p>classic edition - 5$</p>	
+					</div>
+					<button>buy steam</button>
+				</div>
+
+				<div className="edit e2">
+					<div className="classic c2">
+						<p>support edition - 10$</p>	
+					</div>
+					<button>buy steam</button>
+				</div>
+
+				<div className="edit e3">
+					<div className="classic c3">
+						<p>full edition - 15$</p>	
+					</div>
+					<button>buy steam</button>
+				</div>
+
+			</div>
   </div> 
   );  
 }
